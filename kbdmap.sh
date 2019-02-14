37af46722bbe22ed8efae555cd8a4b01839d9046
Vasily Kuznetsov <kvas.it@gmail.com>
2019-02-14 20:19:48 +0100

    Use mac russian layout

--- kbdmap.sh
+++ kbdmap.sh
@@ -2,4 +2,4 @@
 # US and RU keymaps, toggle with Mod4 + space (like Mac ;)
 # See: https://jlk.fjfi.cvut.cz/arch/manpages/man/xkeyboard-config.7
 
-setxkbmap us,ru -option grp:win_space_toggle
+setxkbmap 'us,ru(mac)' -option grp:win_space_toggle
