bf5c6e6ebc4690057a1ff0d552baae08e9a53354
Vasily Kuznetsov <kvas.it@gmail.com>
2019-06-19 14:39:38 +0200

    Add an option to turn the touchpad off

--- touchpad.sh
+++ touchpad.sh
@@ -4,6 +4,7 @@ GETNUM_RE='s/.*(\([0-9]*\)).*/\1/'
 
 TP_ID=$(xinput list --id-only "ETPS/2 Elantech Touchpad")
 
+ON_ID=$(xinput list-props $TP_ID | grep 'Device Enabled (' | sed -e "$GETNUM_RE")
 SPEED_ID=$(xinput list-props $TP_ID | grep 'Accel Speed (' | sed -e "$GETNUM_RE")
 TAP_ID=$(xinput list-props $TP_ID | grep 'Tapping Enabled (' | sed -e "$GETNUM_RE")
 
@@ -11,8 +12,15 @@ TAP_ID=$(xinput list-props $TP_ID | grep 'Tapping Enabled (' | sed -e "$GETNUM_R
 # echo "Speed is $SPEED_ID"
 # echo "Tapping is $TAP_ID"
 
-# Enable tap to click
-xinput set-prop $TP_ID $TAP_ID 1
+if [ "$1" == "off" ]; then
+    xinput set-prop $TP_ID $ON_ID 0
+else
+    # Enable the touchpad.
+    xinput set-prop $TP_ID $ON_ID 1
 
-# Accel-speed (out of 0-1)
-xinput set-prop $TP_ID $SPEED_ID 0.5
+    # Enable tap to click
+    xinput set-prop $TP_ID $TAP_ID 1
+
+    # Accel-speed (out of 0-1)
+    xinput set-prop $TP_ID $SPEED_ID 0.5
+fi
